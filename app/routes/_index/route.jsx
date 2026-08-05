be2ecf1eeaaaac9b2332afabdb2d9135660c1dd8
balvant0707 <snehal@pryxotech.com>
2026-08-05 11:59:11 +0530

    Add new Code 05-08-2026

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -1,21 +1,7 @@
-import { redirect } from "react-router";
-import { isbot } from "isbot";
+import { authenticate } from "../../shopify.server";
 
 export const loader = async ({ request }) => {
-  if (isbot(request.headers.get("User-Agent") || "")) {
-    throw new Response(null, { status: 204 });
-  }
+  await authenticate.admin(request);
 
-  const url = new URL(request.url);
-  const target = new URL("/app", url.origin);
-
-  for (const [key, value] of url.searchParams.entries()) {
-    target.searchParams.append(key, value);
-  }
-
-  throw redirect(`${target.pathname}${target.search}`);
-};
-
-export default function Index() {
   return null;
-}
+};
