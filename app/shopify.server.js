9b30e29b91dde5aa3f4abdbf6dc7d2cee50ca857
balvant0707 <snehal@pryxotech.com>
2026-08-19 10:46:37 +0530

    Add new Code 19-08-2026

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -98,6 +98,9 @@ const shopify = shopifyApp({
   sessionStorage: prismaSessionStorage,
   distribution: AppDistribution.AppStore,
   billing: BILLING_PLANS,
+  future: {
+    expiringOfflineAccessTokens: true,
+  },
   hooks: {
     afterAuth: async ({ session, admin }) => {
       await upsertSessionFromAuth(session);
